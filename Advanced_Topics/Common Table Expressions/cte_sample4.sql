12a12eaec4f23d81feebb31d4b38d5f2fc92cfda
JohnstonKirimo <33547809+JohnstonKirimo@users.noreply.github.com>
2022-04-06 01:15:14 -0400

    Add files via upload

---
+++ Advanced_Topics/Common Table Expressions/cte_sample4.sql
@@ -0,0 +1,23 @@
+--Query Sample 4
+WITH salesrep AS (
+    SELECT
+        employeeNumber,
+        CONCAT(firstName, ' ', lastName) AS salesrepName
+    FROM
+        employees
+    WHERE
+        jobTitle = 'Sales Rep'
+),
+customer_salesrep AS (
+    SELECT
+        customerName, salesrepName
+    FROM
+        customers
+            INNER JOIN
+        salesrep ON employeeNumber = salesrepEmployeeNumber
+)
+SELECT
+    *
+FROM
+    customer_salesrep
+ORDER BY customerName;
