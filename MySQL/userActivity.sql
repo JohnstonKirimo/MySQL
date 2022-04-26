5d2aea85d0820045f58802be0fb06a1ce6e00300
JohnstonKirimo <33547809+JohnstonKirimo@users.noreply.github.com>
2022-04-25 23:53:40 -0400

    Add files via upload

---
+++ MySQL/userActivity.sql
@@ -0,0 +1,34 @@
+/*
+
+Table: Activity
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| session_id    | int     |
+| activity_date | date    |
+| activity_type | enum    |
++---------------+---------+
+There is no primary key for this table, it may have duplicate rows.
+The activity_type column is an ENUM of type ('open_session', 'end_session', 'scroll_down', 'send_message').
+The table shows the user activities for a social media website. 
+Note that each session belongs to exactly one user.
+ 
+
+Write an SQL query to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
+
+Return the result table in any order.
+
+*/
+
+--solution
+select
+    activity_date as day,
+    count(distinct user_id) as active_users
+from 
+    activity
+where
+    datediff('2019-07-27', activity_date) < 30
+group by
+    activity_date;
