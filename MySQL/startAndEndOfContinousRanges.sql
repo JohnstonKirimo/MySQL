69aa23a70821b6207ac8812f7ef16e939fc67334
JohnstonKirimo <33547809+JohnstonKirimo@users.noreply.github.com>
2022-05-03 20:33:35 -0400

    Add files via upload

---
+++ MySQL/startAndEndOfContinousRanges.sql
@@ -0,0 +1,38 @@
+/*
+SQL Schema
+Table: Logs
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| log_id        | int     |
++---------------+---------+
+log_id is the primary key for this table.
+Each row of this table contains the ID in a log Table.
+ 
+
+Write an SQL query to find the start and end number of continuous ranges in the table Logs.
+
+Return the result table ordered by start_id.
+
+*/
+-- sample solution:
+WITH num
+     AS (SELECT log_id,
+                Row_number() OVER (ORDER BY log_id) row_num
+         FROM   logs)
+SELECT Min(log_id) start_id,
+       Max(log_id) end_id
+FROM   num
+GROUP  BY log_id - row_num
+
+-- alternative solution:
+WITH num
+     AS (SELECT log_id,
+                Row_number() OVER (ORDER BY log_id) row_num
+         FROM   logs)
+SELECT Min(log_id) start_id,
+       Max(log_id) end_id
+FROM   num
+GROUP  BY log_id - row_num
+
